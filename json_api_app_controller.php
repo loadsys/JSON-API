b0f8ef907bd152e01841dadf69c6de92cbe6e97a
Joey Trapp <joey@loadsys.com>
2010-10-29 11:58:44 -0500

    removed auto including server response from the plugin app controller

--- json_api_app_controller.php
+++ json_api_app_controller.php
@@ -2,13 +2,6 @@
 
 class JsonApiAppController extends AppController {
 
-	public $components = array(
-		'json_api.ServerResponse',
-		'json_api.RequestData' => array(
-			'form' => true
-		)
-	);
-
 }
 
 ?>
